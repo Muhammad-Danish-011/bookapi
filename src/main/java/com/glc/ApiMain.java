ca98a821b30a2c9ccee48a137463a1f8ab2a3245
Muhammad-Danish-011 <muhammad.danish@xloopdigital.com>
2023-04-07 13:15:05 -0500

    first test fail

--- src/main/java/com/glc/ApiMain.java
+++ src/main/java/com/glc/ApiMain.java
@@ -1,11 +1,21 @@
 package com.glc;
-
+import java.util.*;  
 public class ApiMain {
+
+    public class book {
+            String title ;
+            String auther ;
+            int length ;
+            int year ;
+    }
     
-    public static void main( String[] args )
-             {
-         public  getBook(){
+ 
+       
+    public static void main(String[] args) {          
+        //Create an empty List      
+       // List<String> EmptyList = Collections.<String>emptyList();  
+      //  System.out.println("Empty list: "+EmptyList);  
+        }      
+    }
+          
 
-         }
-
-}
--- src/test/java/com/glc/ApiTest.java
+++ src/test/java/com/glc/ApiTest.java
@@ -2,37 +2,31 @@ package com.glc;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import java.util.*;
 
+import org.junit.Test;
+ 
 
 public class ApiTest 
 {
   
+  
     @Test
     public void TestForEmpty()
     {
-        //Given that I visit the site, when I first start, I expect my list to be empty.
-       //setup
-       List<String> list = new ArrList<String>();
-       List<int> list1 = new Arrlist<int>();
-       //execute
-       for(int i=0; i<list.size() ;i++){
-        System.out.println(list.get(i));
-    } 
-        for(int i=0; i<list.size() ;i++){
-        System.out.println(list1.get(i));
+        //setup
+        List<String> EmptyList = Collections.<String>emptyList(); 
+        //execute 
+        System.out.println("Empty list: "+EmptyList);  
+        //assertion
+            assertEquals(null, EmptyList);
     }
-       //assertion
-    }
+    
 
+}
 
-
-            @Test 
-            public void TestForAddBook(){
-
-            
-            
-            }
+           
+        
 
 
         
